68e766b9821a14bb64b0bcce496422b499c957b9
Bryan Baek <60611815+bryanbaek@users.noreply.github.com>
2024-06-10 16:32:50 +0000

    connect gateay to route for openai

--- app/routes/chat.py
+++ app/routes/chat.py
@@ -1,16 +1,21 @@
 from fastapi import APIRouter, HTTPException, Query
 from app.services.google import get_gemini_pro_response, get_gemini_lite_response
+from app.services.openai import get_gpt_4o_response
+
 
 router = APIRouter()
 
 @router.post("/chat")
-def chat(prompt: str, model: str = Query("gemini-pro", enum=["gemini-pro", "gemini-lite"])):
+def chat(prompt: str, model: str = Query("gemini-pro", enum=["gemini-pro", "gemini-lite", "gpt-4o"])):
     try:
         if model == "gemini-pro":
             response_text = get_gemini_pro_response(prompt)
             return response_text
         elif model == "gemini-lite":
             response_text = get_gemini_lite_response(prompt)
+            return response_text
+        elif model == "gpt-4o":
+            response_text = get_gpt_4o_response(prompt)
             return response_text
         else:
             raise HTTPException(status_code=400, detail="Invalid model specified")
--- app/services/openai.py
+++ app/services/openai.py
@@ -4,7 +4,7 @@ from app.core.config import settings
 # Configuration for OpenAI API
 openai.api_key = settings.OPENAI_API_KEY
 
-def get_chatgpt_4o_response(prompt: str) -> str:
+def get_gpt_4o_response(prompt: str) -> str:
     response = openai.Completion.create(
         engine="gpt-4o",
         prompt=prompt,
